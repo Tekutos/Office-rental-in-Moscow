f47c30c4607afa1514785093ce9d35e2473b70ee
Tekutos <tekuto1993@gmail.com>
2022-11-28 13:31:53 +0300

    finaly first page

--- my-app/src/components/Main/Main.jsx
+++ my-app/src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SocialMedia from "../SocialMedia/SocialMedia";
 import Logo from "./Logo/Logo";
 import "./Main.scss";
 
@@ -22,10 +23,29 @@ function Main() {
             Contact
           </a>
         </div>
+        <span className={`${base}_span-1`}>#49 AMONG BROKERS</span>
+        <span className={`${base}_span-2`}>Office rental in Moscow</span>
+        <div className={`${base}_catalog`}>
+          <span className={`${base}_span-3`}>Go to the catalog</span>
+          <div className={`${base}_ellipse`}></div>
+          <div className={`${base}_arrow`}>
+            <i className="arrow right"></i>
+          </div>
+        </div>
+        <span className={`${base}_span-4`}>News</span>
+        <span className={`${base}_span-5`}>
+          A new skyscraper "825 Third Ave" with 37 offices has been added to our
+          portfolio ...
+        </span>
+        <SocialMedia />
       </div>
-
       <div className={`${base}_mainImg`}></div>
-      <div></div>
+      <div className={`${base}_rentalConditions`}></div>
+      <div className={`${base}_text1`}>Fair rental conditions</div>
+      <div className={`${base}_text2`}>
+        Rent an office for several months or several years without long-term
+        obligations and commissions
+      </div>
     </div>
   );
 }
